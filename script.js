19d64de8fc36166575021023c19d43c7ad02519e
evan-soumya <89275770+evan-soumya@users.noreply.github.com>
2024-08-17 00:29:17 +0530

    Update script-script-toggle-disable.js

--- script.js
+++ script.js
@@ -25,24 +25,3 @@ function copyLink() {
     alert("Website link copied to clipboard!");
 }
 
-// Toggle Menu
-document.addEventListener('DOMContentLoaded', () => {
-    const navToggle = document.querySelector('.nav-toggle');
-    const navMenu = document.querySelector('header nav ul');
-
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.toggle('active');
-    });
-});
-
-// Initialize dynamic text
-const dynamicText = document.querySelector('.dynamic-text');
-const texts = ['Research Scholar', 'Student', 'Science Enthusiast'];
-let index = 0;
-
-function updateText() {
-    dynamicText.textContent = texts[index];
-    index = (index + 1) % texts.length;
-    setTimeout(updateText, 3000);
-}
-updateText();
